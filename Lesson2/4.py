0ac61977ba69490286718595b4f73a1922441338
Andrey <ab@inteb.pro>
2020-09-11 11:51:11 +0400

    Добавил пробелы)

--- Lesson2/4.py
+++ Lesson2/4.py
@@ -1,7 +1,6 @@
 user_str = input("Введите предложение из нескольких слов \n")
 
-
 list_user_str = user_str.split(" ")
 
 for ind, el in enumerate(list_user_str):
-    print(ind +1, el[0:10])
+    print(ind + 1, el[0:10])
